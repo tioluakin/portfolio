2ef875a82a4f35dba8864cc2465c5aca733ea1ae
tioluakin <tioluakin@gmail.com>
2024-03-31 15:52:54 +0100

    still making changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+port:10000,
+    host: '0.0.0.0',
+
+  },
+  
+});
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
